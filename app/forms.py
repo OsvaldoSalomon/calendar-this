9ec85047152ee64c2b4c5b8239893e6458eb8158
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2022-06-14 19:31:20 -0400

    Added custom validation

--- app/forms.py
+++ app/forms.py
@@ -1,19 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 from wtforms.fields import (
     BooleanField, DateField, StringField, SubmitField, TextAreaField, TimeField
 )
-
-validatorRequired = [DataRequired()]
+from datetime import datetime
 
 class AppointmentForm(FlaskForm):
-    name = StringField("Name", validatorRequired)
-    start_date = DateField("Start", validatorRequired)
-    start_time = TimeField("Start", validatorRequired)
-    end_date = DateField("End", validatorRequired)
-    end_time = TimeField("End", validatorRequired)
-    description = TextAreaField("Description", validatorRequired)
+    name = StringField("Name", validators=[DataRequired()])
+    start_date = DateField("Start", validators=[DataRequired(), ])
+    start_time = TimeField("Start", validators=[DataRequired()])
+    end_date = DateField("End", validators=[DataRequired()])
+    end_time = TimeField("End", validators=[DataRequired()])
+    description = TextAreaField("Description", validators=[DataRequired()])
     private = BooleanField("Private?")
     submit = SubmitField("Create Appointment")
+
+    def validate_end_date(form, field):
+        start = datetime.combine(form.start_date.data, form.start_time.data)
+        end = datetime.combine(field.data, form.end_time.data)
+        start_date = form.start_date.data
+        end_date = form.end_date.data
+        if start >= end:
+            msg = "End date/time must come after start date/time"
+            raise ValidationError(msg)
+        elif start_date != end_date:
+            msg = "End date should be on the same day as the start date"
+            raise ValidationError(msg)
 
 
